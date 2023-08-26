f8026168e3b8e3858bcc3ddea82d4b49aa5c526f
DESKTOP-B4SGJAI\shira <shiraba28@gmail.com>
2023-08-26 15:09:06 +0300

    ml-agents enemy

---
+++ Assets/Scripts/MLAgent/MoveToTargetAgent.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToTargetAgent : Agent
+{
+    [SerializeField] private Transform target;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-3.5f, -1.5f), Random.Range(-3.5f, 3.5f));
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation((Vector2)transform.localPosition);
+        sensor.AddObservation((Vector2)target.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        // Debug.Log(actions.ContinuousActions.Length);
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float movementSpeed = 2f;
+
+        transform.position += new Vector3(moveX, moveY) * Time.deltaTime * movementSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            EndEpisode();
+        }
+        else if (collision.TryGetComponent(out Wall wall))
+        {
+            AddReward(-2f);
+            EndEpisode();   
+        }
+    }
+
+
+}
